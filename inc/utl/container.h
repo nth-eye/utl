b889f7a4f395539898bd146c0b64105e6eebe566
Ilya Makarov <ilya.makarov.592@gmail.com>
2022-06-28 21:10:10 +0300

    feat: static vectors for run/compile time

--- inc/utl/container.h
+++ inc/utl/container.h
@@ -2,64 +2,46 @@
 #define UTL_CONTAINER_H
 
 #include "utl/base.h"
+#include <array>
 
 namespace utl {
+namespace detail {
 
-/**
- * @brief Uninitialized storage container for any type.
- * 
- * @tparam T Type of the element.
- */
-template<class T>
-struct storage {
-    // operator T*() const { return reinterpret_cast<const T*>(buf); }
-    operator T*()       { return reinterpret_cast<T*>(buf); } 
-    // auto operator T() const   { return reinterpret_cast<const T*>(buf); }
-    // auto operator T()         { return reinterpret_cast<T*>(buf); } 
-private:
-    alignas(T) unsigned char buf[sizeof(T)];
-};
+template<class T, size_t N, class Storage>
+struct svector_base {
 
-/**
- * @brief Resizable static vector with pre-allocated memory on stack.
- * 
- * @tparam T Type of elements
- * @tparam N Maximum number of elements
- */
-template<class T, size_t N>
-struct svector {
     // Get capacity of the vector.
-    constexpr size_t capacity() const       { return N; }
+    constexpr size_t capacity() const               { return N; }
     // Get size of the vector.
-    constexpr size_t size() const           { return len; }
+    constexpr size_t size() const                   { return len; }
     // Check if the vector is empty.
-    constexpr bool empty() const            { return len == 0; }
+    constexpr bool empty() const                    { return len == 0; }
     // Check if the vector is full.
-    constexpr bool full() const             { return len == N; }
+    constexpr bool full() const                     { return len == N; }
     // Get element at index.
-    constexpr T& operator[](size_t i) const { return buf[i]; }
+    constexpr const T& operator[](size_t i) const   { return buf[i]; }
     // Get element at index without const.
-    constexpr T& operator[](size_t i)       { return buf[i]; }
+    constexpr T& operator[](size_t i)               { return buf[i]; }
     // Get pointer to the beginning of the vector.
-    constexpr T* begin() const              { return buf[0]; }
+    constexpr const T* begin() const                { return buf; }
     // Get pointer to the beginning of the vector.
-    constexpr T* begin()                    { return buf[0]; }
+    constexpr T* begin()                            { return buf; }
     // Get pointer to the end of the vector.
-    constexpr T* end() const                { return buf[len]; }
+    constexpr const T* end() const                  { return buf + len; }
     // Get pointer to the end of the vector.
-    constexpr T* end()                      { return buf[len]; }
+    constexpr T* end()                              { return buf + len; }
     // Get pointer to the beginning of the vector.
-    constexpr T* data() const               { return buf; }
+    constexpr const T* data() const                 { return buf; }
     // Get pointer to the beginning of the vector.
-    constexpr T* data()                     { return buf; }
+    constexpr T* data()                             { return buf; }
     // Clear the vector.
-    constexpr void clear()                  { len = 0; }
+    constexpr void clear()                          { len = 0; }
     // Resize the vector.
-    constexpr void resize(size_t size)      { if (size <= N) len = size; }
+    constexpr void resize(size_t size)              { if (size <= N) len = size; }
     // Push element to the end of the vector.
-    constexpr void push_back(const T &x)    { if (len < N) *buf[len++] = x; }
+    constexpr void push_back(const T &x)            { if (len < N) buf[len++] = x; }
     // Pop element from the end of the vector.
-    constexpr void pop_back()               { if (len > 0) --len; }
+    constexpr void pop_back()                       { if (len > 0) --len; }
     // Erase element at iterator.
     constexpr void erase(T *p)
     {
@@ -68,9 +50,67 @@ struct svector {
         *p = buf[len];
     }
 private:
-    storage<T> buf[N];
+    Storage buf;
     size_t len = 0;
 };
+
+}
+
+/**
+ * @brief Uninitialized storage container for any type. Uses raw byte array
+ * as underlying storage. Because of this, cannot be used in constexpr 
+ * context, but objects constructed on demand and not at "allocation" time.
+ * Converts implicitly to pointer of underlying type.
+ * 
+ * @tparam T Type of elements.
+ * @tparam N Number of elements.
+ */
+template<class T, size_t N>
+struct storage {
+    operator auto()         { return reinterpret_cast<T*>(buf); }
+    operator auto() const   { return reinterpret_cast<const T*>(buf); }
+private:
+    alignas(T) unsigned char buf[sizeof(T)][N];
+};
+
+/**
+ * @brief Constexpr storage container for any type. Uses regular C array 
+ * as underlying storage. Because of this, objects are always default 
+ * constructed at "allocation" time, but can be used in constexpr context. 
+ * Converts implicitly to pointer of underlying type.
+ * 
+ * @tparam T Type of elements.
+ * @tparam N Number of elements.
+ */
+template<class T, size_t N>
+struct ce_storage {
+    constexpr operator auto()       { return static_cast<T*>(buf); }
+    constexpr operator auto() const { return static_cast<const T*>(buf); }
+private:
+    T buf[N]{};
+};
+
+/**
+ * @brief Resizable static vector with pre-allocated memory on stack.
+ * Uses utl::storage, so no constexpr support, but doesn't default 
+ * construct elements. More efficient than utl::ce_svector at run-time.
+ * 
+ * @tparam T Type of elements
+ * @tparam N Maximum number of elements
+ */
+template<class T, size_t N>
+struct svector : detail::svector_base<T, N, storage<T, N>> {};
+
+/**
+ * @brief Resizable static vector with pre-allocated memory on stack.
+ * Uses utl::ce_storage, so has constexpr support at the cost of default 
+ * object construction. Less efficient than utl::svector at run-time.
+ * 
+ * @tparam T Type of elements
+ * @tparam N Maximum number of elements
+ */
+template<class T, size_t N>
+struct ce_svector : detail::svector_base<T, N, ce_storage<T, N>> {};
 
 }
 
--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 #include "utl/utl.h"
 
-// struct Ctor {
-// private:
-//     Ctor()
-//     {
-//         printf("ctor \n");
-//     }
-// };
+struct Ctor {
+    Ctor(int) {}
+    Ctor()
+    {
+        printf("ctor \n");
+    }
+};
 
-int main()
+constexpr auto test_ce_vector()
 {
-    utl::svector<int, 5> vec;
+    utl::ce_svector<int, 5> vec = {};
 
     vec.push_back(1);
     vec.push_back(2);
@@ -19,7 +19,14 @@ int main()
     vec.push_back(5);
     vec.push_back(6);
 
-    for (auto &x : vec)
+    return vec;
+}
+
+int main()
+{
+    constexpr auto vec = test_ce_vector();
+
+    for (const auto &x : vec)
         printf("%d ", x);
 
     return 0;
